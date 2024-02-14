de0474089ea2869f1cf2531926f0ac22133b2b08
Molina289 <147972410+Molina289@users.noreply.github.com>
2024-02-14 23:02:15 +0800

    Update models.py

--- server/groups/models.py
+++ server/groups/models.py
@@ -22,3 +22,43 @@ class Group(models.Model):
             self.members.remove(member)
         else:
             print("未发现此成员。")
+
+class Notification(models.Model):
+    """
+    通知
+    """
+    publisher = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="publisher",
+        verbose_name="发布者",
+    )
+
+    content = models.TextField(verbose_name="通知内容")
+
+    class Meta:
+        app_label = "groups"
+        db_table = "zq_notify_group"
+        verbose_name = "通知"
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return f"{self.content}"
+
+
+class Attendance(models.Model):
+    """
+    签到
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="user",
+        verbose_name="签到成员",
+    )
+
+    attendance = models.BooleanField(
+        default=False
+    )
+
+# Create your models here.
