1426c6b5762d63849758a4f2fdc08a5453cc7d9f
ctf狗都不打 <148362728+fwweber@users.noreply.github.com>
2024-02-16 17:58:07 +0800

    feat:Todo的model和view的显示任务及提醒功能

--- server/todos/models.py
+++ server/todos/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextFeild(blank=True)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    reminder = models.DateTimeField(null=True,blank=True)
+    deadline = models.DateTimeField(null=True, blank=True) 
+
+    def __str__(self):  
+        return self.title
